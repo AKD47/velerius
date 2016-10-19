3dc467be482dadf139c799a7dc559c42ae93c611
AKD47 <van83horn@gmail.com>
2016-10-19 17:02:08 +0300

    sub-menu script

---
+++ app/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    /*sub-menu show*/ 
+    $('.nav-top li').click(function () { 
+        /*console.log('click');*/
+        var subBlock = $(this).find('.sub-menu');
+        var shevron = $(this).find('.shevron');
+        /*$('.sub-menu').hide();*/
+        if (subBlock.hasClass('open')) {
+            subBlock.removeClass('open').css({display: 'none'});
+            shevron.css({display: 'none'});
+        }
+        else {
+            $('.sub-menu').removeClass('open').hide();
+            $('.shevron').hide();
+            subBlock.addClass('open').css({display: 'block'});
+            shevron.css({display: 'block'});            
+        };
+    });
+
+    /*close sub-menu show*/
+
+});
