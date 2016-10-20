da731025d434bf70edb40df09ca843c3fe700f71
dmitryi <dmitryi.zavadskyi@yandex.ru>
2016-10-20 10:37:43 +0300

    empty theme

---
+++ template-parts/content-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Velerius
+ */
+
+?>
+
+<?php the_title(); ?>
+<?php the_content(); ?>
